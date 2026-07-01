574ed5f1cd7f171126cbbe761ef0566869b2b7b1
PedroHRamosRitter <pedrohenriqueramosritter@gmail.com>
2026-07-01 19:45:22 -0300

    Remove repositorios embutidos e adicionas arquivos da aula 22 e 27

--- Modulo 02/Aulas/Aula 27/js/cadastro.js
+++ Modulo 02/Aulas/Aula 27/js/cadastro.js
@@ -3,8 +3,11 @@ const form = document.querySelector('#form-cadastro')
 // Registrar o usuário ao enviar o formulário
 
 form.addEventListener('submit', (evento) => {
-    
-    
-
+    evento.preventDefault() // Impede o carregamento da página
+    const usuario = {
+        email: document.querySelector('#email').value,
+        senha: document.querySelector('#senha').value
+    }
+    console.log(usuario)
 }
 )
