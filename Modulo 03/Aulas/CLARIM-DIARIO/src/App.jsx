bd493c5e4bf881ba44c84fe8c93f48c29a9f7065
PedroHRamosRitter <pedrohenriqueramosritter@gmail.com>
2026-07-13 20:48:43 -0300

    Adicionando main

--- Modulo 03/Aulas/CLARIM-DIARIO/src/App.jsx
+++ Modulo 03/Aulas/CLARIM-DIARIO/src/App.jsx
@@ -1,10 +1,48 @@
 import Header from './components/Header/Header'
+<<<<<<< HEAD
 
 
 function App() {
   return (
     <>
       <Header />
+=======
+import NewsCard from './components/NewsCard/NewsCard'
+import { noticias } from './data/noticias'
+import './App.css'
+
+
+function App() {
+  const [manchete, ...demais] = noticias
+
+
+  return (
+    <>
+      <Header />
+
+      <main className='container'>
+        <section className='manchete'>
+          <NewsCard 
+          categoria={manchete.categoria} 
+          titulo={manchete.titulo} 
+          resumo={manchete.resumo}>
+
+          </NewsCard>
+        </section>
+
+        <section className='grade'>
+          {demais.map((noticia) => [
+            <NewsCard
+            key={noticia.id}
+            categoria={noticia.categoria}
+            titulo={noticia.titulo}
+            resumo={noticia.resumo}
+          />
+          ])}
+        </section>
+      </main>
+
+>>>>>>> ecb1a27 (Adicionando main)
     </>
 
   )
