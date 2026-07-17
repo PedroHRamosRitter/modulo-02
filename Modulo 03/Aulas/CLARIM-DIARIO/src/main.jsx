18f3f4a0e0cd4103f1e347b111a23bd8576e8e57
PedroHRamosRitter <pedrohenriqueramosritter@gmail.com>
2026-07-17 19:12:35 -0300

    Adicionando alteracao de cor entre claro e escuro

--- Modulo 03/Aulas/CLARIM-DIARIO/src/main.jsx
+++ Modulo 03/Aulas/CLARIM-DIARIO/src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-
 import './styles/global.css'
 import './styles/variables.css'
 // "achando" a div de id - root e entraga o controle dela pro react
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    
   </StrictMode>,
 )
